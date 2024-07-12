a38e81f5e5816a8703e90d1cb36144ba6b626ef8
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-12 20:13:10 +0530

    Update solution.py

--- 21 Days Of Code/Day 01/Problem 1/solution.py
+++ 21 Days Of Code/Day 01/Problem 1/solution.py
@@ -1,12 +1,1 @@
-#Write a program to print "Hello Coding Blocks !" on the screen.
-
-Input Format
-Constraints
-Output Format
-Hello Coding Blocks !
-
-Sample Input
-Sample Output
-Hello Coding Blocks !#
-
 print("Hello Coding Blocks !")
