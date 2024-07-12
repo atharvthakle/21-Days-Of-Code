95b615280095c674563d44eb1d9c007086cb7236
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 01:39:53 +0530

    Update solution.py

--- 21 Days Of Code/Day 04/Problem 4/solution.py
+++ 21 Days Of Code/Day 04/Problem 4/solution.py
@@ -1,24 +1,3 @@
-#Take N (number of rows), print the following pattern (for N = 4).
-
-                       1 
-                     2 3 2
-                   3 4 5 4 3
-                 4 5 6 7 6 5 4
-Input Format
-Constraints
-0 < N < 10
-
-Output Format
-Sample Input
-4
-
-Sample Output
-            1
-		2	3	2
-	3	4	5	4	3
-4	5	6	7	6	5	4#
-
-
 def print_pattern(N):
     # Outer loop for rows
     for i in range(1, N + 1):
