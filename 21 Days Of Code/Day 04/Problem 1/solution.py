6ed62d06fc695123288cc9d54dfb526b1e921727
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 01:27:37 +0530

    Update solution.py

--- 21 Days Of Code/Day 04/Problem 1/solution.py
+++ 21 Days Of Code/Day 04/Problem 1/solution.py
@@ -1,32 +1,3 @@
-#You will be given a number N. You have to code a hollow diamond looking pattern.
-
-The output for N=5 is given in the following image.
-
-Screen Shot 2016-06-09 at 2.47.18 pm.png
-
-Input Format
-The input has only one single integer N.
-
-Constraints
-Output Format
-Output the given pattern.
-
-Sample Input
-5
-Sample Output
-*********
-**** ****
-***    ***
-**      **
-*         *
-**      **
-***    ***
-**** ****
-*********
-Explanation
-Write a code to print above given pattern. No space between any two characters.#
-
-
 w = int(input())
 count = w
 k = 0
