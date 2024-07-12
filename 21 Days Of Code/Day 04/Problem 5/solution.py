0cd9d69606f945ebfb1e23acd804f841d40c0330
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 01:42:09 +0530

    Update solution.py

--- 21 Days Of Code/Day 04/Problem 5/solution.py
+++ 21 Days Of Code/Day 04/Problem 5/solution.py
@@ -1,23 +1,3 @@
-#Take N (number of rows), print the following pattern (for N = 4).
-
-                       1           1
-                       1 2       2 1  
-                       1 2 3   3 2 1
-                       1 2 3 4 3 2 1   
-Input Format
-Constraints
-0 < N < 10
-
-Output Format
-Sample Input
-4
-Sample Output
-1						1
-1	2				2	1
-1	2	3		3	2	1
-1	2	3	4	3	2	1#
-
-
 n = int(input())
 
 if n % 2 == 0:
