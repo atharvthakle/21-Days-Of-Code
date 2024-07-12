759498d02695c65ed4dbb9a5565dce3aa9527384
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-12 20:15:23 +0530

    Update solution.py

--- 21 Days Of Code/Day 01/Problem 2/solution.py
+++ 21 Days Of Code/Day 01/Problem 2/solution.py
@@ -1,25 +1,3 @@
-#Help Lakshya Bhaiya recite tables of an input number x to the limit x*y.
-
-Print the table of x from x*1, x * 2, x * 3… x * y.
-
-Input Format
-Take two inputs, x and y
-
-Constraints
-x<=10,000 y<=10,000
-
-Output Format
-each line contains the table of the number x
-
-Sample Input
-2 4
-Sample Output
-2
-4
-6
-8#
-
-
 # Read input
 x, y = map(int, input().split())
 
