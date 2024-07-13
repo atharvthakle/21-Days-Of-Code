eb937edc6cfff29578bb627240c4e95747ea8a8b
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 20:30:35 +0530

    Update solution.py

--- 21 Days Of Code/Day 09/Problem 3/solution.py
+++ 21 Days Of Code/Day 09/Problem 3/solution.py
@@ -1,21 +1,3 @@
-#Sort just 0 and 1
-
-Input Format
-A line containing N number of 0s and 1s Next line follows a long sequence of 0 and 1 seperated by space
-
-Constraints
-N will not exceed 10^7
-
-Output Format
-Sorted Sequence
-
-Sample Input
-7
-1 0 0 1 1 0 1
-Sample Output
-0 0 0 1 1 1 1#
-
-
 def sort_zeros_and_ones(arr):
     count_zeros = arr.count(0)
     count_ones = len(arr) - count_zeros
