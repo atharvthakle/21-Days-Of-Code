6d3dc9b0b71803a94b2793aa6b4620ed550ea657
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 20:32:55 +0530

    Update solution.py

--- 21 Days Of Code/Day 09/Problem 5/solution.py
+++ 21 Days Of Code/Day 09/Problem 5/solution.py
@@ -1,30 +1,3 @@
-#You will be given an array containing only 0s, 1s and 2s. you have sort the array in linear time that is O(N) where N is the size of the array.
-
-Input Format
-The first line contains N, which is the size of the array. The following N lines contain either 0, or 1, or 2.
-
-Constraints
-1 <= N <= 10^6
-Each input element x, such that x ∈ { 0, 1, 2 }.
-
-Output Format
-Output the sorted array with each element separated by a newline.
-
-Sample Input
-5
-0
-1
-2
-1
-2
-Sample Output
-0
-1
-1
-2
-2#
-
-
 def sort_012(arr):
     low, mid, high = 0, 0, len(arr) - 1
     
