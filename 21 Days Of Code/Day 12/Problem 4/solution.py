9aee1d042be594e6ff08b3cbb2f03f7bc94f82de
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 21:54:40 +0530

    Update solution.py

--- 21 Days Of Code/Day 12/Problem 4/solution.py
+++ 21 Days Of Code/Day 12/Problem 4/solution.py
@@ -1,24 +1,3 @@
-#You have to reverse the first k elements in the Queue.
-
-Input Format
-First line contains the N-the number of elements. Second line contains the N elements. Take input the value of K.
-
-Constraints
-Try to solve it in linear time.
-
-Output Format
-Print the queue after doing the above task.
-
-Sample Input
-5 
-1 2 3 4 5
-3
-Sample Output
-3 2 1 4 5
-Explanation
-After reversing the given input from the 3rd position the resultant output will be 3 2 1 4 5.#
-
-
 from collections import deque
 
 def reverse_k_elements(queue, k):
