b6bf1d2e955f3855df880fb9114d1e96eb7947db
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 21:53:37 +0530

    Update solution.py

--- 21 Days Of Code/Day 12/Problem 3/solution.py
+++ 21 Days Of Code/Day 12/Problem 3/solution.py
@@ -1,26 +1,3 @@
-#The Game is as follows You have given a binary array, where 1 denotes push operation and 0 denotes a pop operation in a queue. The task is to check if the possible set of operations are valid or not.
-Print Valid if the set of Operations are Valid Otherwise Print Invalid.
-
-Input Format
-The First Line contains an Integer T, as the number of Test cases.
-The Next Line contains an Integer N, as the Size of the Array.
-The Next Line contains N Binary numbers separated by space.
-
-Constraints
-Output Format
-Print Valid If the set of operations are valid Otherwise Print Invalid for each Test Case separated by a new Line.
-
-Sample Input
-2
-5
-1 1 0 0 1
-5
-1 1 0 0 0 
-Sample Output
-Valid
-Invalid#
-
-
 def is_valid_operations(operations):
     count = 0
     for op in operations:
