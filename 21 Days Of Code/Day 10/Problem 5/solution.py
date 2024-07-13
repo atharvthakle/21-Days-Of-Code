a6b44b6f1c3b2fa4cf9e118b375ebbd12e15918f
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2024-07-13 20:54:33 +0530

    Update solution.py

--- 21 Days Of Code/Day 10/Problem 5/solution.py
+++ 21 Days Of Code/Day 10/Problem 5/solution.py
@@ -1,24 +1,3 @@
-#Given a linked list containing 0’s, 1’s, 2’s, sort the linked list by doing a single traversal of it.
-
-Input Format
-First line of input contains integer N, denoting the size of linked list. Next line of input contains N space separated integers, denoting the elements of the linked list.
-
-Constraints
-1<=N<=10^4
-
-Output Format
-Print the elements of the linked list after sorting.
-
-Sample Input
-12
-0 1 2 2 1 0 0 2 0 1 1 0
-Sample Output
-0 0 0 0 0 1 1 1 1 2 2 2 
-Explanation
-0 0 0 0 0 1 1 1 1 2 2 2 linked list after sorting.#
-
-
-
 class Node:
     def __init__(self, data):
         self.data = data
